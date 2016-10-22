64dfd3de5cfc16bf6ef0475e2bf55b99688c3935
EFGH Technologies <email@efgh-technologies.com>
2016-10-22 20:07:09 +0530

    services page functionality upto real estate working

--- js/myjquery-scripts.js
+++ js/myjquery-scripts.js
@@ -1,16 +1,3 @@
 // JavaScript Document
 
 
-$(document).ready(function() 
-{
- $('#prop-button').click(function() {
-
-      $('#prop-dd').toggle();
-  });
- $('#bedroom-button').click(function() {
-
-      $('#bedroom-dd').toggle();
-  });
-
-  
-});

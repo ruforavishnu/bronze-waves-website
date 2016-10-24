be91a25268d5aa4c87be7b13f3194ded40d0597d
EFGH Technologies <email@efgh-technologies.com>
2016-10-24 15:13:26 +0530

    added search fn

---
+++ search.php
@@ -0,0 +1,281 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Bronze waves</title>
+
+    <link href="css/reset.css" rel="stylesheet">  
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">  
+
+    <!-- Custom CSS -->
+    <link href="css/modern-business.css" rel="stylesheet"> 
+    <link href="css/styles.css" rel="stylesheet"> 
+
+    <link href="css/searchbar-styles.css" rel="stylesheet"> 
+
+    <!-- mobirise css -->
+    <link rel="stylesheet" href="css/mobirise/style.css"> 
+
+    <!-- end of mobirise css-->
+    
+    
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Alegreya" rel="stylesheet"> 
+    
+
+    <!-- Custom Fonts -->
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css"> 
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    
+    	<script type ="text/javascript" src="js/jquery-3.1.1.min.js"></script> 
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>	
+	<script src="https://use.fontawesome.com/b94ecde39a.js"></script> 
+
+    <script type="text/javascript" src="js/smoothscroll.js"></script>
+    <script type="text/javascript" src="js/myjquery-scripts.js"></script> 
+    
+    <link rel="stylesheet" href="css/lightbox.min.css">
+    
+
+<!--
+<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAd9NVkMYFqtUckT0aPC2VrOEaVZh5g8N0"></script>
+ <script type="text/javascript" src="js/jquery.googlemap.js"></script>-->
+ 
+<!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet"/>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+-->
+
+</head>
+
+<body class="font-alegreya">
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-fixed-top  navbar-props" role="navigation">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+
+                <img src="img/logo.jpg" class="logo-img">
+                <a class="navbar-brand text-black logoname-align" href="index.html"><h1>Bronze Waves</h1></a>
+
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right navbar-pad-up ">
+                  	<li>
+                    	<a class="hypertext-red smoothScroll nav-bg" href="index.html">Home</a>
+                    </li>
+                    <li>
+                        <a class="hypertext-red smoothScroll nav-bg" href="index.html#about-us">About Us</a>
+                    </li>
+                    <li>
+                        <a class="hypertext-red smoothScroll nav-bg" href="services.html">Services</a>
+                    </li>
+					<li class="dropdown">
+                        <a href="#" class="dropdown-toggle hypertext-red smoothScroll nav-bg" data-toggle="dropdown">Divisions <b class="caret"></b></a>
+                        <ul class="dropdown-menu">
+                            <li>
+                                <a href="block-factory.html">Block Factory</a>
+                            </li>
+                            <li>
+                                <a href="logistics.html">Logistics</a>
+                            </li>
+                            <li>
+                                <a href="real-estate.html">Real Estate</a>
+                            </li>
+                            <li>
+                                <a href="laundry.html">Laundry</a>
+                            </li>
+
+                        </ul>
+                    </li>
+					<li>
+                        <a class="hypertext-red smoothScroll nav-bg" href="index.html#careers">Careers</a>
+                    </li>
+                    <li>
+                        <a class="hypertext-red smoothScroll nav-bg" href="index.html#contact-us">Contact Us</a>
+                    </li>
+                </ul>
+            </div>
+                                 
+
+            <!-- /.navbar-collapse -->
+<div class="row">
+            	<div class="col-lg-12">
+<!-- search form 1 -->
+
+                
+                </div>
+            </div>
+
+        </div>
+        <!-- /.container -->
+
+        
+	            
+    </nav>
+    <section id="back-img">
+    </section>
+    <div class="container ">
+    <div class="row">
+    <div class="col-lg-12 ">
+    <br><br><br>
+</div>
+    </div>
+<div class="row">
+
+
+ <div class="col-md-3 ">
+                  <h4 class="block-shape text-center force-bg-white"  >Search Results for 
+                  <span style="color:#db232f">
+				  <?php
+				  $keyword = $_POST['search-keyword'];
+				  echo '"';
+				   echo $keyword;
+				   echo '"';
+				   ?>
+                   </span>
+                   </h4> 
+ </div>
+ <div class="col-md-3 ">
+                 
+ </div>
+ <div class="col-md-3 ">
+                 
+ </div>
+ <div class="col-md-3 ">
+                
+ </div>   
+</div>
+                                                          
+
+                  </div>
+               
+                 <div  class="force-bg-white" id="service-section-block">
+                 <div class="container">
+                 <div class="row"  >
+<br><br>            
+               
+                 
+  <div class="col-md-6">
+  <style>
+  
+.myfancybox-img
+  {
+  display:inline-block;
+	  
+  }
+  </style>
+
+
+<div id="one-bhk-gallery">
+
+<a class="example-image-link" href="img/one-bhk/one-bhk(1).jpg" data-lightbox="example-1">
+<img class="example-image myfancybox-img" src="img/one-bhk/one-bhk(1).jpg" alt="image-1" />
+</a>
+<a class="example-image-link" href="img/one-bhk/one-bhk(2).jpg" data-lightbox="example-1">
+<img class="example-image myfancybox-img" src="img/one-bhk/one-bhk(2).jpg" alt="image-1" />
+</a>
+<a class="example-image-link" href="img/one-bhk/one-bhk(3).jpg" data-lightbox="example-1">
+<img class="example-image myfancybox-img" src="img/one-bhk/one-bhk(3).jpg" alt="image-1" />
+</a>
+<a class="example-image-link" href="img/one-bhk/one-bhk(4).jpg" data-lightbox="example-1">
+<img class="example-image myfancybox-img" src="img/one-bhk/one-bhk(4).jpg" alt="image-1" />
+</a>
+<a class="example-image-link" href="img/one-bhk/one-bhk(5).jpg" data-lightbox="example-1">
+<img class="example-image myfancybox-img" src="img/one-bhk/one-bhk(5).jpg" alt="image-1" />
+</a>
+<a class="example-image-link" href="img/one-bhk/one-bhk(6).jpg" data-lightbox="example-1">
+<img class="example-image myfancybox-img" src="img/one-bhk/one-bhk(6).jpg" alt="image-1" />
+</a>
+<a class="example-image-link" href="img/one-bhk/one-bhk(7).jpg" data-lightbox="example-1">
+<img class="example-image myfancybox-img" src="img/one-bhk/one-bhk(7).jpg" alt="image-1" />
+</a>
+<a class="example-image-link" href="img/one-bhk/one-bhk(8).jpg" data-lightbox="example-1">
+<img class="example-image myfancybox-img" src="img/one-bhk/one-bhk(8).jpg" alt="image-1" />
+</a>
+
+		</div>
+         <script language="javascript">
+        $(document).ready(function()
+		{
+
+
+	
+        });
+        </script>
+            </div>  
+            <div class="col-md-6 ">
+
+                <p class="text-justify" >
+                
+                <strong>                Darsit  (1bhk)</strong><br>
+<ol>
+
+<li>1 bedroom </li>
+<li>1 sitting room</li>
+<li>2 bathrooms </li>
+<li>Kitchen </li>
+<li>Balcony </li>
+<li>Including maintenance </li>
+<li>No commission</li>
+<br>
+<li>250/= RO</li>
+</ol>
+<br>
+
+                <strong>For more details Contact</strong><br>
+c.ahmed112@yahoo.com<br>
+Tel: +968- 24587012
+<br>
+                </p>
+</div>
+</div>
+
+        </div>
+
+
+        </div><!--service-secton-block-->
+		
+        
+      
+        
+
+
+ 
+
+                                                    
+
+        <script>
+$(document).ready(function() {
+    
+
+
+});
+
+</script>
+<script src="js/lightbox-plus-jquery.min.js"></script>
+  
+    </body>
+</html>
